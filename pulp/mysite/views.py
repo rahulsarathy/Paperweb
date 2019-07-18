2f0ea630e1b455ee31c98c57c4f9aa6d0a4cd0a0
Rahul Sarathy <rahul@sarathy.org>
2019-07-17 19:09:25 -0700

    fix views

--- pulp/mysite/views.py
+++ pulp/mysite/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("hello world")
+    return render(request, 'mysite/index.html')
