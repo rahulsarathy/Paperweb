efc9bcd0b6e5bdc4874c9e166f66eb68fba061fa
Rahul Sarathy <rahul@sarathy.org>
2019-07-21 01:45:33 -0700

    create book

--- pulp/static/js/src/components/Magazine.jsx
+++ pulp/static/js/src/components/Magazine.jsx
@@ -13,9 +13,18 @@ export default class Magazine extends React.Component {
 
 	render () {
     return (
+        <div>
     	<div className="magazine">
-            
+            <div className="binding"></div>
+            <div className="content">
+                <h1 className="magazine-title">PULP</h1> 
+                <p className="subtitle">July 1st to July 31st</p>
+                <p className="subtitle">Harry Daly</p>
+            </div>
     	</div>
+        <div className="page"></div>
+        <div className="page2"></div>
+        </div>
     	);
   }
 }
