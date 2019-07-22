ab4bb69395a9129a163641923c31f9f1784dd6ed
Rohit Sarathy <rohit@sarathy.org>
2019-07-21 23:45:03 -0700

    s/mysite/siteconfig; local server can run successfully now.

--- siteconfig/urls.py
+++ siteconfig/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from siteconfig import views
 
-from mysite import views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
