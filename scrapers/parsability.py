af1c3b5223b2bbfa0fda0b88a565d98858c6cf44
Rahul Sarathy <rahul@sarathy.org>
2019-07-25 19:36:02 -0700

    use feedparser

--- scrapers/parsability.py
+++ scrapers/parsability.py
@@ -58,7 +58,7 @@ class Scraper(object):
 class Article(object):
 
     def __init__(self, name=None, date_published=None, author=None, author_bio=None, author_profile_image=None,
-                 comments=[],):
+                 comments=[], permalink=None, content_link=None):
 
         self.name = name
         self.date_published = date_published
@@ -66,11 +66,25 @@ class Article(object):
         self.author_bio = author_bio
         self.author_profile_image = author_profile_image
         self.comments = comments
+        self.permalink= permalink
+        self.content_link = content_link
 
     def add_comment(self, comment):
 
         self.comments.append(comment)
         return self.comments
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'date_published': self.date_published,
+            'author': self.author,
+            'author_bio': self.author_bio,
+            'author_profile_image': self.author_profile_image,
+            'comments': len(self.comments),
+            'permalink': self.permalink,
+            'content_link': self.content_link
+        }
 
 
 class Comment(object):
