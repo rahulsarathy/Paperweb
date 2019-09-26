59c35561b10699350c6e92e2041a779dbd900545
Rahul Sarathy <rahul@sarathy.org>
2019-09-26 17:58:18 -0400

    finish landing

--- static/js/src/landing/landing.jsx
+++ static/js/src/landing/landing.jsx
@@ -44,35 +44,55 @@ export default class Landing extends React.Component {
     	<div className="landing">
             <h1 className="logo">Pulp</h1>
             <h3 className="questioncopy">What if the internet published a magazine?</h3>
-            <div>
+            <div className="blogcopy">
                 <p>Pulp is a monthly custom print magazine delivered to your doorstep made up of your favorite blogs and newsletters that you choose.</p>
             </div>
-            <button>Get Started</button>
-            <div className="blogcopy">
+            <button className="getstarted"><a href="/api/users/signup">Get Started</a></button>
+            <div className="contentcopy">
                 <p>The internet's best writing delivered to your doorstep</p>
             </div>
-            <Row>
-                <div className="blogrow">
-                {
-                    this.state.row1.map((blog) => 
-                        <BlogCard key={shortid.generate()} blog={blog}/>
-                        )
-                } 
-                </div>
-            </Row>
-            <Row>
-                <div className="blogrow">
-                {
-                    this.state.row2.map((blog) => 
-                        <BlogCard key={shortid.generate()} blog={blog}/>
-                        )
-                } 
-                </div>
-            </Row>
+            <div className="bloglist">
+                <Row>
+                    <Col>
+                        <p>Technology</p>
+                        <ul>
+                            <li><a target="_blank" href="https://stratechery.com/">Stratechery</a></li>
+                            <li><a target="_blank" href="https://kwokchain.com/">kwokchain</a></li>
+                            <li><a target="_blank" href="https://breakingsmart.com/en/season-1/">breakingsmart</a></li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <p>Economics</p>
+                        <ul>
+                            <li><a target="_blank" href="https://www.econlib.org/author/bcaplan/">Bryan Caplan</a></li>
+                            <li><a target="_blank" href="https://marginalrevolution.com/">Marginal Revolution</a></li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <p>Think Tanks</p>
+                        <ul>
+                            <li><a target="_blank" href="https://www.mercatus.org/">Mercatus Center</a></li>
+                            <li><a target="_blank" href="https://www.aei.org/">AEI</a></li>
+                            <li><a target="_blank" href="https://www.hoover.org/">Hoover Institution</a></li>
+                            <li><a target="_blank" href="https://www.cato.org/">Cato Institute</a></li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <p>Rationality</p>
+                        <ul>
+                            <li><a target="_blank" href="https://www.ribbonfarm.com/">ribbonfarm</a></li>
+                            <li><a target="_blank" href="https://slatestarcodex.com/">SlateStarCodex</a></li>
+                            <li><a target="_blank" href="https://meltingasphalt.com/">Melting Asphalt</a></li>
+                            <li><a target="_blank" href="http://www.overcomingbias.com/">Overcoming Bias</a></li>                    
+                        </ul>
+                    </Col>
+                </Row>
+            </div>
     	</div>
     	);
   }
 }
+
 
 ReactDOM.render(<Landing/>, document.getElementById('landing'))
 
