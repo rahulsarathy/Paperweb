653e7fa7a1fbbe70420e0e52c90da269192f87a8
Rahul Sarathy <rahul@sarathy.org>
2019-10-06 02:39:01 -0400

    find latest celery

--- blogs/tasks.py
+++ blogs/tasks.py
@@ -1,5 +1,7 @@
 from celery import task
 from celery import shared_task
+from utils.blog_utils import SCRAPERS
+import logging
 
 # We can have either registered task
 @task(name='summary')
@@ -11,3 +13,10 @@ def send_import_summary():
 def send_notifiction():
      print('Here I\’m')
      # Another trick
+
+@task(name='find_latest')
+def find_latest():
+     for scraper in SCRAPERS:
+          to_fire = scraper()
+          logging.warning("Firing {}".format(to_fire.name_id))
+          scraper().poll()
