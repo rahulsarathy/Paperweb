16a60cdea2a74ad22c27cbb840058cf47996b627
Rahul Sarathy <rahul@sarathy.org>
2019-11-02 12:43:42 -0400

    nodemon autorefresh

--- node/server.js
+++ node/server.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const Mercury = require('@postlight/mercury-parser');
 const express = require('express');
 
 // Constants
@@ -9,11 +9,14 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello worl1d\n');
 });
 
-app.post('/mercury', (req, res) => {
-  res.send('Hello world tes32t23ing1\n');
+app.post('api/mercury', (req, res) => {
+  console.log("hit api mercury");
+  // var url = req.body.url;
+  // console.log(url);
+  res.send('Hello worwaddsddsald tes3223432t23ing1\n');
 });
 
 app.listen(PORT, HOST);
