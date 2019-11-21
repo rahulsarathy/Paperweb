8d3d3696ad75f898ae4e4e156da7a912350ed536
Vijay Sarathy <developer@sarathy.org>
2019-11-21 15:55:32 -0800

    removing DJANGO_ALLOWED_HOSTS

--- backend/pulp/settings.py
+++ backend/pulp/settings.py
@@ -29,8 +29,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
 # Application definition
 
 INSTALLED_APPS = [
