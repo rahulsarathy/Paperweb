b010350425bdb31fdbd8e1cd94e75d84632dec7d
Rahul Sarathy <rahul@sarathy.org>
2019-11-22 02:10:02 -0500

    change password

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,7 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUser(AbstractUser):
-    email_activated = models.BooleanField(_('Activated Through Email'), default=False)
 
     def __str__(self):
         return self.email
