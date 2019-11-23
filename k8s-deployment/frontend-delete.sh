ab8bc6a10f1401c15459baab124d114507e9a2d0
Vijay Sarathy <developer@sarathy.org>
2019-11-23 00:42:56 -0800

    updated k8s deploy spec files

--- k8s-deployment/frontend-delete.sh
+++ k8s-deployment/frontend-delete.sh
@@ -2,4 +2,3 @@
 
 kubectl delete service frontend 
 kubectl delete deployment frontend
-kubectl delete persistentvolumeclaim frontend-claim-volume
--- k8s-deployment/frontend-startup.sh
+++ k8s-deployment/frontend-startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-kubectl create -f ./frontend-claim.yaml
 kubectl create -f ./frontend-deployment.yaml
 kubectl create -f ./frontend-service.yaml
