e066dff81ec6cae558dcb8baeb1b219fbc115391
Rahul Sarathy <rahul@sarathy.org>
2019-11-27 15:55:19 -0800

    fixed untracked files

--- backend/app/pulp/settings.py
+++ backend/app/pulp/settings.py
@@ -198,10 +198,10 @@ SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles"),
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "../static")
 ]
 
 LOGGING = {
