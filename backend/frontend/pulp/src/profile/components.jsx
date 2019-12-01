6407265c8d9b954bca4215a39be330ccca8e8921
Rahul Sarathy <rahul@sarathy.org>
2019-12-01 01:45:32 -0800

    lowercase using git

--- backend/frontend/pulp/src/profile/components.jsx
+++ backend/frontend/pulp/src/profile/components.jsx
@@ -1,19 +1,13 @@
 import './components.scss'
 import Address_Pane from './address_pane.jsx';
-import Cancel_Pane from './cancel_pane.jsx';
-import Payment_Pane from './payment_pane.jsx';
 import Address_Modal from './address_modal.jsx';
-import Paid from './paid.jsx';
 import Unpaid from './unpaid.jsx';
 import Header from './header.jsx';
 
 
 export {
 	Address_Pane,
-	Cancel_Pane,
-	Payment_Pane,
 	Address_Modal,
 	Unpaid,
-	Paid,
 	Header
 }
