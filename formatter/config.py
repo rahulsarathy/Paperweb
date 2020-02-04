8ab992e06a9f524eb71c05addfb52fc369eea2de
Rahul Sarathy <rahul@sarathy.org>
2020-02-03 21:45:35 -0500

    working flask in docker

--- formatter/config.py
+++ formatter/config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = True
-    SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'testing')
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
