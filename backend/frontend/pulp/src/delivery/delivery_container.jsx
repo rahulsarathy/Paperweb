67668309e305092d431f45f6d93849c0f4375322
Rahul Sarathy <rahul@sarathy.org>
2020-03-04 16:10:09 -0500

    move reading list to delivery_container

--- backend/frontend/pulp/src/delivery/delivery_container.jsx
+++ backend/frontend/pulp/src/delivery/delivery_container.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { Table, DropdownButton, Dropdown } from "react-bootstrap";
 import { TableContainer, PageCount, Filter } from "./components.jsx";
+import $ from "jquery";
 
 export default class DeliveryContainer extends React.Component {
 	constructor(props) {
@@ -9,9 +10,11 @@ export default class DeliveryContainer extends React.Component {
 
 		this.handleSearch = this.handleSearch.bind(this);
 		this.changeSort = this.changeSort.bind(this);
+		this.changeDeliver = this.changeDeliver.bind(this);
 
 		this.state = {
-			sort: "date_added"
+			sort: "date_added",
+			reading_list: []
 		};
 	}
 
@@ -21,6 +24,48 @@ export default class DeliveryContainer extends React.Component {
 
 	handleSearch(e) {
 		this.setState({ search: e.target.value });
+	}
+
+	componentDidMount() {
+		this.getReadingList();
+	}
+
+	getReadingList() {
+		var csrftoken = $("[name=csrfmiddlewaretoken]").val();
+		let data = {
+			csrfmiddlewaretoken: csrftoken
+		};
+		$.ajax({
+			url: "../api/reading_list/get_reading",
+			data: data,
+			type: "GET",
+			success: function(data) {
+				this.setState({
+					reading_list: data
+				});
+				console.log(data[0].to_deliver);
+			}.bind(this)
+		});
+	}
+
+	changeDeliver(to_deliver, permalink) {
+		var csrftoken = $("[name=csrfmiddlewaretoken]").val();
+		let data = {
+			to_deliver: !to_deliver,
+			permalink: permalink,
+			csrfmiddlewaretoken: csrftoken
+		};
+		$.ajax({
+			url: "../api/reading_list/update_deliver",
+			data: data,
+			type: "POST",
+			success: function(data) {
+				this.setState({
+					reading_list: data
+				});
+				console.log(data[0].to_deliver);
+			}.bind(this)
+		});
 	}
 
 	render() {
@@ -33,9 +78,9 @@ export default class DeliveryContainer extends React.Component {
 				<PageCount reading_list={this.props.reading_list} />
 				<TableContainer
 					search={this.state.search}
-					reading_list={this.props.reading_list}
+					reading_list={this.state.reading_list}
 					sort={this.state.sort}
-					changeDeliver={this.props.changeDeliver}
+					changeDeliver={this.changeDeliver}
 					changeSort={this.changeSort}
 				/>
 			</div>
--- backend/frontend/pulp/src/delivery/table_container.jsx
+++ backend/frontend/pulp/src/delivery/table_container.jsx
@@ -1,59 +1,11 @@
 import React, { Component } from "react";
 import { TableHeader, DeliveryItems } from "./components.jsx";
-import $ from "jquery";
 
 export default class TableContainer extends Component {
 	constructor(props) {
 		super(props);
 
-		this.changeDeliver = this.changeDeliver.bind(this);
-		this.getReadingList = this.getReadingList.bind(this);
-
-		this.state = {
-			reading_list: []
-		};
-	}
-
-	componentDidMount() {
-		this.getReadingList();
-	}
-
-	getReadingList() {
-		var csrftoken = $("[name=csrfmiddlewaretoken]").val();
-		let data = {
-			csrfmiddlewaretoken: csrftoken
-		};
-		$.ajax({
-			url: "../api/reading_list/get_reading",
-			data: data,
-			type: "GET",
-			success: function(data) {
-				this.setState({
-					reading_list: data
-				});
-				console.log(data[0].to_deliver);
-			}.bind(this)
-		});
-	}
-
-	changeDeliver(to_deliver, permalink) {
-		var csrftoken = $("[name=csrfmiddlewaretoken]").val();
-		let data = {
-			to_deliver: !to_deliver,
-			permalink: permalink,
-			csrfmiddlewaretoken: csrftoken
-		};
-		$.ajax({
-			url: "../api/reading_list/update_deliver",
-			data: data,
-			type: "POST",
-			success: function(data) {
-				this.setState({
-					reading_list: data
-				});
-				console.log(data[0].to_deliver);
-			}.bind(this)
-		});
+		this.state = {};
 	}
 
 	render() {
@@ -64,10 +16,10 @@ export default class TableContainer extends Component {
 					sort={this.props.sort}
 				/>
 				<DeliveryItems
-					reading_list={this.state.reading_list}
+					reading_list={this.props.reading_list}
 					search={this.props.search}
 					sort={this.props.sort}
-					changeDeliver={this.changeDeliver}
+					changeDeliver={this.props.changeDeliver}
 				/>
 			</div>
 		);
