46ec7c16534645f07c96cf97a728c20e2e797743
Rahul Sarathy <rahul@sarathy.org>
2020-03-15 21:15:43 -0400

    start instapaper api

--- backend/app/reading_list/instapaper.py
+++ backend/app/reading_list/instapaper.py
@@ -1,13 +1,15 @@
-from getpass import getpass
-from bs4 import BeautifulSoup
-import csv
-import mechanicalsoup
-from reading_list.utils import add_to_reading_list, get_reading_list
-from django.http import HttpResponse
-from reading_list import tasks
 import os
+import csv
+
 from reading_list.models import InstapaperCredentials
+from reading_list import tasks
+from reading_list.utils import add_to_reading_list, get_reading_list
+
+from bs4 import BeautifulSoup
+import mechanicalsoup
+from django.http import HttpResponse
 from django.utils import timezone
+import requests
 
 def import_from_instapaper(user, username, password):
     # use credentials to sign into instapaper
@@ -42,3 +44,15 @@ def import_from_instapaper(user, username, password):
     tasks.parse_instapaper_csv.delay(final_list, user.email)
 
     return HttpResponse(status=200)
+
+def instapaper_api(username, password):
+    authenticate_url = 'https://www.instapaper.com//api/1/oauth/access_token'
+
+    data = {
+        'x_auth_username': username,
+        'x_auth_password': password,
+        'x_auth_mode': 'client_auth',
+    }
+    response = requests.get(authenticate_url, data=data)
+    print(response)
+    return response
