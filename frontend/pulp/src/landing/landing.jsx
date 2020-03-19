9a9afeb16496d0cf4265d711d14ba83bedb75b06
Rahul Sarathy <rahul@sarathy.org>
2020-03-19 12:01:32 -0700

    formatted landing.jsx, render into id not body

--- frontend/pulp/src/landing/landing.jsx
+++ frontend/pulp/src/landing/landing.jsx
@@ -8,24 +8,20 @@ import "./components.scss";
 const images_url = "../static/images/";
 
 function Header() {
-	return (
-		<div id="header">
-			<img
-				className="logo"
-				src={images_url + "pulp_black_logo.svg"}
-			/>
-			<div className="links">
+    return (
+        <div id="header">
+            <img className="logo" src={images_url + "pulp_black_logo.svg"} />
+            <div className="links">
                 <a href="#">Pricing</a>
-				<a href="#">Contact</a>
+                <a href="#">Contact</a>
                 <a href="#">FAQ</a>
-				<span>|</span>
-				<a href="/accounts/login">Login</a>
-				<a href="/accounts/signup">Sign Up</a>
-			</div>
-		</div>
-	);
+                <span>|</span>
+                <a href="/accounts/login">Login</a>
+                <a href="/accounts/signup">Sign Up</a>
+            </div>
+        </div>
+    );
 }
-
 
 function Splash() {
     return (
@@ -34,7 +30,10 @@ function Splash() {
                 <h1>Your favorite articles, made into a magazine</h1>
             </Col>
             <Col className="right">
-                <h4>High quality writing was meant to be held, not scrolled through</h4>
+                <h4>
+                    High quality writing was meant to be held, not scrolled
+                    through
+                </h4>
                 <Button id="sign-up">Sign Up</Button>
             </Col>
         </Row>
@@ -47,20 +46,27 @@ function Steps() {
             <Row>
                 <Col className="step">
                     <div className="step-info">
-                        <img src={images_url + "step1.png"}/>  
-                        <span className="step-desc">Add Articles you want to read</span>
+                        <img src={images_url + "step1.png"} />
+                        <span className="step-desc">
+                            Add Articles you want to read
+                        </span>
                     </div>
                 </Col>
                 <Col className="step">
                     <div className="step-info">
-                        <img src={images_url + "step2.png"}/>  
-                        <span className="step-desc">Read, whenever you want</span>
+                        <img src={images_url + "step2.png"} />
+                        <span className="step-desc">
+                            Read, whenever you want
+                        </span>
                     </div>
                 </Col>
                 <Col className="step">
                     <div className="step-info">
-                        <img src={images_url + "step3.png"}/>  
-                        <span className="step-desc">What you don't finish gets delivered to you as a magazine</span>
+                        <img src={images_url + "step3.png"} />
+                        <span className="step-desc">
+                            What you don't finish gets delivered to you as a
+                            magazine
+                        </span>
                     </div>
                 </Col>
             </Row>
@@ -71,7 +77,7 @@ function Steps() {
 function Preview() {
     return (
         <div id="preview">
-            <img src={images_url + "preview.png"}/>
+            <img src={images_url + "preview.png"} />
         </div>
     );
 }
@@ -79,12 +85,12 @@ function Preview() {
 function Landing() {
     return (
         <div>
-            <Header/>
-            <Splash/>
-            <Steps/>
-            <Preview/>
+            <Header />
+            <Splash />
+            <Steps />
+            <Preview />
         </div>
     );
 }
 
-ReactDOM.render(<Landing />, document.querySelector("body"));
+ReactDOM.render(<Landing />, document.getElementById("landing"));
