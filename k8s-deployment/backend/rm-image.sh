4ffd461890a495591a905a6ac613bc7a07d677e1
Rahul Sarathy <rahul@sarathy.org>
2020-03-23 17:14:00 -0700

    update project name

--- k8s-deployment/backend/rm-image.sh
+++ k8s-deployment/backend/rm-image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcloud container images delete gcr.io/sandbox-164917/pulp-backend:v1 --force-delete-tags
+gcloud container images delete gcr.io/prod1-259305/pulp-backend:v1 --force-delete-tags
