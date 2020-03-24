c11c9b53aba6055f886b43bf6441a4aa5b4145b2
Rahul Sarathy <rahul@sarathy.org>
2020-03-23 20:27:32 -0700

    remove celery services

--- k8s-deployment/celery-beat/setup.sh
+++ k8s-deployment/celery-beat/setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 kubectl create -f ./pulp-celery-beat-deployment.yaml
-kubectl create -f ./pulp-celery-beat-service.yaml
--- k8s-deployment/celery/setup.sh
+++ k8s-deployment/celery/setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 kubectl create -f ./pulp-celery-deployment.yaml
-kubectl create -f ./pulp-celery-service.yaml
