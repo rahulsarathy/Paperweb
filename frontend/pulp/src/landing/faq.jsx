f8c89a5e70bfd3e606d7cd51363049bfecb94308
Rahul Sarathy <rahul@sarathy.org>
2020-03-28 02:11:31 -0700

    preliminary FAQ questions

--- frontend/pulp/src/landing/faq.jsx
+++ frontend/pulp/src/landing/faq.jsx
@@ -2,6 +2,22 @@ import React, { Component } from "react";
 
 export default class FAQ extends Component {
 	render() {
-		return <div>faq</div>;
+		return (
+			<div>
+				<p>What is Pulp?</p>
+
+				<p>How much does Pulp cost?</p>
+
+				<p>How often do I receive Pulp magazines?</p>
+
+				<p>How can I cancel my subscription?</p>
+
+				<p>What countries do you deliver to?</p>
+
+				<p>Are PDF files supported?</p>
+
+				<p>Where can I send my feature requests and suggestions?</p>
+			</div>
+		);
 	}
 }
