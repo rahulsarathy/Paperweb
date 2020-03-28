3bc520bdec933efbe3063e249326f9e2ac4d7077
Rahul Sarathy <rahul@sarathy.org>
2020-03-28 02:04:18 -0700

    publisher rough draft message

--- frontend/pulp/src/landing/publishers.jsx
+++ frontend/pulp/src/landing/publishers.jsx
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 
 export default class Publishers extends Component {
 	render() {
-		return <div>publisher</div>;
+		return (
+			<div>
+				We want to pay publishers. For every magazine that contains one
+				of your articles, you will receive a payout. Contact us at
+				rahul@getpulp.io to find out more. Most publishers value the
+				increased engagement and revenue that Pulp brings to their
+				content. However, if you would not like your articles to be
+				printed out in Pulp magazines, contact us at rahul@getpulp.io so
+				we can blacklist the internet domain names of your sites.
+			</div>
+		);
 	}
 }
