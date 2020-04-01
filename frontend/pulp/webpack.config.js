9e035c42b1db3560395ac066d44a0d3385245e91
Robert Morrison <robbieguy98@gmail.com>
2020-04-01 10:13:06 -0400

    Add a webpack entry to package up the refactored code

--- frontend/pulp/webpack.config.js
+++ frontend/pulp/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     landing: path.join(__dirname, "src", "landing", "landing.jsx"),
     switcher: path.join(__dirname, "src", "switcher", "switcher.jsx"),
     article: path.join(__dirname, "src", "article", "article.jsx"),
-    subscribe: path.join(__dirname, "src", "subscribe", "subscribe.jsx")
+    subscribe: path.join(__dirname, "src", "subscribe", "subscribe.jsx"),
+    testing: path.join(__dirname, "src", "dashboard")
   },
   output: {
     path: path.join(__dirname, "build"),
@@ -43,6 +44,9 @@ module.exports = {
         use: "file-loader"
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
   plugins: [
     new HtmlWebpackPlugin({
