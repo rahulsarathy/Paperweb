afb630b94da2292a251c998d3965c39bb5472f5d
Rahul Sarathy <rahul@sarathy.org>
2020-04-03 01:25:54 -0700

    - remove worker command - fix get_channel_layer typo

--- backend/app/pulp/asgi.py
+++ backend/app/pulp/asgi.py
@@ -4,4 +4,4 @@ from channels.asgi import get_channel_layer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pulp.prod_settings')
 
-channel_layer = get_channel_layer
+channel_layer = get_channel_layer()
