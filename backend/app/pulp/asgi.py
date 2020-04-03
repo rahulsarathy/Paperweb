d27f4441fdac230831587321beeb0924d5872587
Rahul Sarathy <rahul@sarathy.org>
2020-04-03 01:16:39 -0700

    update daphne deploy command

--- backend/app/pulp/asgi.py
+++ backend/app/pulp/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from channels.asgi import get_channel_layer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pulp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pulp.prod_settings')
 
 channel_layer = get_channel_layer
