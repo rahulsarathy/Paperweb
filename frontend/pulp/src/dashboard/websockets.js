3fc8732da52c720ae99603c26e98bf648c218a64
Robert Morrison <robbieguy98@gmail.com>
2020-04-05 19:58:33 -0400

    Add a skeleton of the websockets code

--- frontend/pulp/src/dashboard/websockets.js
+++ frontend/pulp/src/dashboard/websockets.js
@@ -1,7 +1,3 @@
-var initialState = {
-    loading: []
-}
-
 export function connectToWebSocket(dispatch) {
     return new Promise(function (resolve, reject) {
         const progressSocket = new WebSocket("ws://" + window.location.host + "/ws/api/progress/")
@@ -19,39 +15,8 @@ function onMessage(dispatch) {
         let data = JSON.parse(message.data)
 
         switch (data.job_type) {
-            case "add_to_reading_list":
-                dispatch({ type: "UPDATE_ARTICLE_PERCENTAGE", url: data.link, percent: data.percent })
-                return
             default:
                 return
         }
-    }
-}
-export function reducer(state = initialState, action) {
-    switch (action.type) {
-        case "UPDATE_ARTICLE_PERCENTAGE":
-            if (action.percent === 100) {
-                return Object.assign({}, state, {
-                    loading: state.loading.filter((item) => item.url !== action.url)
-                })
-            } else if (action.percent === 0) {
-                return Object.assign({}, state, {
-                    loading: [...state.loading, { url: action.url, percent: action.percent }]
-                })
-            } else {
-                return Object.assign({}, state, {
-                    loading: state.loading.map((item) => {
-                        if (item.url === action.url) {
-                            return Object.assign({}, item, {
-                                percent: action.percent
-                            })
-                        } else {
-                            return item
-                        }
-                    })
-                })
-            }
-        default:
-            return state;
     }
 }
